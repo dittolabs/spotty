e2b5c368be62cd17ff46d26e50760052e12d7a3b
Minh Phan <dmphan@ditto.us.com>
2015-10-28 03:17:30 +0000

    Add handling for another possible response case when calling url

--- lib/spotty.rb
+++ lib/spotty.rb
@@ -23,10 +23,23 @@ class Spotty
   end
 
   def handle_response(res)
+    # According to the spot termination documentation notice documentation
+    # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/spot-interruptions.html
+    # if an instance is scheduled for termination, calling the meta-data url
+    # will return in a response with a time value, e.g. '2015-01-05T18:02:00Z'.
+    # if an instance is NOT scheduled for termination, the response can either
+    # be an HTTP 404 error or have a value that is not a time value.
+    
     case res
     when Net::HTTPSuccess then
-      @continue_poll = false
-      yield
+      time_value = res.body.lines.grep(/.*T.*Z/)
+      if time_value.empty?
+        puts "200 returned, but no time value. Spot not scheduled for termination"
+        sleep(@interval)
+      else
+        @continue_poll = false
+        yield
+      end
     when Net::HTTPNotFound
       puts "404 returned. Spot not scheduled for termination"
       sleep(@interval)
